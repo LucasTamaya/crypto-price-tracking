86eb8032ec1a6ed0c42485a6c04224c07183d730
LucasTamaya <lucastamaya.974@gmail.com>
2022-10-06 12:29:23 +0400

    Update BASE_URL on production mode

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,4 +1,6 @@
 export const COINGECKO_API_URL = "https://api.coingecko.com/api/v3/coins";
 
 export const BASE_URL =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
+  process.env.NODE_ENV === "production"
+    ? "https://crypto-price-tracking-lucastamaya.vercel.app"
+    : "http://localhost:3000";
